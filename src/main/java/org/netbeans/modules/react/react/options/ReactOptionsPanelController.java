532d6d68c3f4e685bdec7b5aa25dd64e18d7940c
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-01-22 10:53:47 +0100

    Start of Options panel.

---
+++ src/main/java/org/netbeans/modules/react/react/options/ReactOptionsPanelController.java
@@ -0,0 +1,84 @@
+package org.netbeans.modules.react.react.options;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(
+        location = "Html5",
+        displayName = "#AdvancedOption_DisplayName_React",
+        keywords = "#AdvancedOption_Keywords_React",
+        keywordsCategory = "Html5/React"
+)
+@org.openide.util.NbBundle.Messages({
+    "AdvancedOption_DisplayName_React=React.js", 
+    "AdvancedOption_Keywords_React=React"})
+public final class ReactOptionsPanelController extends OptionsPanelController {
+
+    private ReactPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    public void applyChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                getPanel().store();
+                changed = false;
+            }
+        });
+    }
+
+    public void cancel() {
+        // need not do anything special, if no changes have been persisted yet
+    }
+
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    public boolean isChanged() {
+        return changed;
+    }
+
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private ReactPanel getPanel() {
+        if (panel == null) {
+            panel = new ReactPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
