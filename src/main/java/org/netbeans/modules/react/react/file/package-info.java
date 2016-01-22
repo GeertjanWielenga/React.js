da00c3b1f7a1262eb554ecca1617afa86d2ffef9
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-01-22 12:15:23 +0100

    Adding description file.

--- src/main/java/org/netbeans/modules/react/react/file/package-info.java
+++ src/main/java/org/netbeans/modules/react/react/file/package-info.java
@@ -1,7 +1,9 @@
 @TemplateRegistration(
         folder = "Other", 
+        category = "HTML5",
         content = "JSXTemplate.jsx", 
         requireProject = false, 
+        description = "JSXDescription.html",
         displayName = "JSX File")
 package org.netbeans.modules.react.react.file;
 
