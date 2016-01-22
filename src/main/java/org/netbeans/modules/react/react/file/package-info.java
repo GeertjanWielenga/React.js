b68d7864e3a503b181835d31d308a47704614eef
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-01-22 11:48:52 +0100

    Registering file template.

--- src/main/java/org/netbeans/modules/react/react/file/package-info.java
+++ src/main/java/org/netbeans/modules/react/react/file/package-info.java
@@ -1,4 +1,8 @@
-@TemplateRegistration(folder = "Other", content = "JSXTemplate.jsx")
+@TemplateRegistration(
+        folder = "Other", 
+        content = "JSXTemplate.jsx", 
+        requireProject = false, 
+        displayName = "JSX File")
 package org.netbeans.modules.react.react.file;
 
 import org.netbeans.api.templates.TemplateRegistration;
