d0bc9ee585a9ec82d84730dc175b14ff46553f1b
Chrizzly <christian.lenz@gmx.net>
2016-06-14 21:27:28 +0200

    Added code templates for React Basic and Stateless Component.

---
+++ src/main/java/org/netbeans/modules/react/react/templates/ReactBasicComponent.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author ${user}
+ * Date: ${date}, ${time}
+ */
+import React, {PropTypes} from 'react';
+
+class ${name} extends React.Component {
+    render() {
+    
+    }    
+}
+
+${name}.propTypes = {};
+${name}.defaultProps = {};
+
+export default ${name};
