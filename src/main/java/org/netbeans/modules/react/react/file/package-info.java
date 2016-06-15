c4fed14d5f8d124b577813c00d127895a3678be0
Chris <chrl@spreadshirt.net>
2016-06-15 10:08:15 +0200

    Added the TemplateRegistration to the package-info.java file which was given before.

--- src/main/java/org/netbeans/modules/react/react/file/package-info.java
+++ src/main/java/org/netbeans/modules/react/react/file/package-info.java
@@ -1,9 +1,29 @@
-@TemplateRegistration(
+@TemplateRegistrations({
+    @TemplateRegistration(
         folder = "ClientSide", 
         content = "JSXTemplate.jsx", 
         requireProject = false, 
         description = "JSXDescription.html",
-        displayName = "JSX File")
+        displayName = "JSX File"
+    ),
+    @TemplateRegistration(
+        folder = "ClientSide",
+        content = "ReactBasicComponent.js",
+        requireProject = false,
+        displayName = "React Basic Component",
+        iconBase = "org/netbeans/modules/react/react/file/react.png",
+        scriptEngine = "javascript"
+    ),
+    @TemplateRegistration(
+        folder = "ClientSide",
+        content = "ReactStatelessComponent.js",
+        requireProject = false,
+        displayName = "React Stateless Component",
+        iconBase = "org/netbeans/modules/react/react/file/react.png",
+        scriptEngine = "javascript"
+    )
+})
 package org.netbeans.modules.react.react.file;
 
 import org.netbeans.api.templates.TemplateRegistration;
+import org.netbeans.api.templates.TemplateRegistrations;
